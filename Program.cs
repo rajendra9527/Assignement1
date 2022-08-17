367e0be1343b60f793cbd61cb0e3f51ca2ace07f
rajendra9527 <rajendranaidu9527@gmail.com>
2022-08-17 14:53:44 +0530

    Changes for Assignment 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Demo;
+using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Hello World";
+            Console.WriteLine("Length of the string is" + " "+str1.Length);
+
+            Demo1 d = new Demo1();
+            d.message();
+            Console.ReadLine();
+        }
+    }
+}
