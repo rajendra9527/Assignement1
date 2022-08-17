6f2f014d3f81931e8a5024b558c960e495914757
rajendra9527 <rajendranaidu9527@gmail.com>
2022-08-17 14:56:24 +0530

    Changes for Demo Assignment

---
+++ Demo/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo
+{
+    public class Demo1
+    {
+       string str1;
+        public string message()
+        {
+            str1 = "You are the Great";
+            Console.WriteLine(str1);
+            return str1;
+        }
+    }
+}
